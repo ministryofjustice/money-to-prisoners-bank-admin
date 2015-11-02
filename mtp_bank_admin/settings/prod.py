357f938e2e797180206890416a7b23b0abea3f4f
Igor <igor.ushkarev@digital.justice.gov.uk>
2015-11-02 15:06:02 +0000

    replaces missing production/docker options (to be like cashbook)

--- mtp_bank_admin/settings/prod.py
+++ mtp_bank_admin/settings/prod.py
@@ -1,3 +1,18 @@
+"""
+Production/Docker settings
+"""
 from .base import *
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = [
+    'localhost',
+    '.dsd.io',
+    '.service.gov.uk'
+]
 
 ADI_TEMPLATE_FILEPATH = '/var/local/adi_template.xlsx'
