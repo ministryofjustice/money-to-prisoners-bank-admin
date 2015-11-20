314eb9f543e81663a20b042d6a96003e72e1dcec
Max Froumentin <max.froumentin@digital.justice.gov.uk>
2015-11-20 13:40:58 +0000

    Use the new mojularised common assets

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+/* jshint node: true */
+
+var webpack = require('webpack');
+
+module.exports = {
+  entry: {
+    app: './mtp_bank_admin/assets-src/javascripts/main.js',
+    polyfills: ['JSON2', 'html5shiv']
+  },
+  output: {
+    path: './mtp_bank_admin/assets/scripts',
+    filename: '[name].bundle.js'
+  },
+  module: {
+    loaders: [
+      { include: /\.json$/, loaders: ['json-loader'] }
+    ],
+    noParse: [
+      /\.\/node_modules\/checked-polyfill\/checked-polyfill\.js$/
+   ]
+  },
+  resolve: {
+    root: [
+      __dirname + '/node_modules',
+      __dirname + '/node_modules/mojular-moj-elements/node_modules'
+    ],
+    modulesDirectories: [
+      'node_modules',
+      'node_modules/money-to-prisoners-common/assets/javascripts/modules'
+    ],
+    extensions: ['', '.json', '.js']
+  },
+  plugins: [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery'
+    })
+  ]
+};
