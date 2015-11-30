a32e3a15ec9b162f6c1934b13c90d4577f29360d
Igor <igor.ushkarev@digital.justice.gov.uk>
2015-11-30 11:02:09 +0000

    Present date list in the natural UK format

--- mtp_bank_admin/apps/bank_admin/templatetags/dates.py
+++ mtp_bank_admin/apps/bank_admin/templatetags/dates.py
@@ -1,15 +1,13 @@
-from datetime import date, timedelta
+from datetime import timedelta
 
 from django import template
+from django.utils.timezone import now
 
 register = template.Library()
 
 
 @register.assignment_tag
-def get_preceding_days(number_of_days, offset=0, format_string='%Y-%m-%d'):
-    days = []
-    current = date.today() - timedelta(days=offset)
-    for _ in range(number_of_days):
-        days.append(current.strftime(format_string))
-        current = current - timedelta(days=1)
+def get_preceding_days(number_of_days, offset=0):
+    current = now().date()
+    days = [current - timedelta(days=day) for day in range(offset, offset + number_of_days)]
     return days
