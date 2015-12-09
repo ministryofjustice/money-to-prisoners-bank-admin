a6c044158ca5a214d3514686a3c7499eed4f7a53
Igor <igor.ushkarev@digital.justice.gov.uk>
2015-12-09 12:32:58 +0000

    Add ping.json view for IRaT support

--- mtp_bank_admin/urls.py
+++ mtp_bank_admin/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import include, url
 from django.core.urlresolvers import reverse_lazy
 
 from moj_auth import views
+from moj_utils.views import PingJsonView
 
 urlpatterns = [
     url(r'^login/$', views.login, {
@@ -15,4 +16,10 @@ urlpatterns = [
     ),
     url(r'^', include('bank_admin.urls', namespace='bank_admin',)),
     url(r'^', include('feedback.urls')),
+
+    url(r'^ping.json$', PingJsonView.as_view(
+        build_date_key='APP_BUILD_DATE',
+        commit_id_key='APP_GIT_COMMIT',
+        version_number_key='APP_BUILD_TAG',
+    ), name='ping_json'),
 ]
