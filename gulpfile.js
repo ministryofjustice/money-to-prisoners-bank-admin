4ce5c99a8309e182e1ed6d02375b175654152b03
Max Froumentin <max.froumentin@digital.justice.gov.uk>
2015-12-15 11:36:22 +0000

    Remove using the minified stylesheets because: 1. The minified stylesheets (generated using clean-css) break the style on IE8 2. They only give a 2% gain in file size Until we investigate a new minified that works on IE8 or we (wait for a) fix in clean-css, we choose just to remove it. [Updates: #110145316]

--- gulpfile.js
+++ gulpfile.js
@@ -76,10 +76,6 @@ gulp.task('sass', ['clean:css'], getTask('scss', {
   browserSync: production ? browserSync : false
 }));
 
-gulp.task('minify-css', ['sass'], getTask('minify-css', {
-  src: paths.dest + 'stylesheets/**/*.css',
-  dest: paths.dest + 'stylesheets'
-}));
 
 gulp.task('scripts', ['clean:js'], function(callback) {
   webpack(require('./webpack.config.js')).run(function(err, stats) {
@@ -121,7 +117,7 @@ gulp.task('images', ['clean:images'], getTask('images', {
 
 
 gulp.task('build', [
-  'minify-css',
+  'sass',
   'minify-scripts',
   'images'
 ]);
