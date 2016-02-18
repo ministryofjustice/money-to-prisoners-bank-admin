d632febdb1b13730e11e3966dd955426e110c923
Ian Brechin <ian.brechin@gmail.com>
2016-02-18 15:02:59 +0000

    Allow users to change their passwords
    Adds URL routing to the views provided by the django-moj-auth
    library and templates in money-to-prisoners-common.

--- mtp_bank_admin/urls.py
+++ mtp_bank_admin/urls.py
@@ -5,12 +5,26 @@ from moj_auth import views
 from moj_irat.views import HealthcheckView, PingJsonView
 
 urlpatterns = [
-    url(r'^login/$', views.login, {'template_name': 'mtp_auth/login.html'}, name='login'),
+    url(
+        r'^login/$', views.login, {
+            'template_name': 'mtp_auth/login.html'
+        }, name='login'
+    ),
     url(
         r'^logout/$', views.logout, {
             'template_name': 'mtp_auth/login.html',
             'next_page': reverse_lazy('login'),
         }, name='logout'
+    ),
+    url(
+        r'^password_change/$', views.password_change, {
+            'template_name': 'auth/password_change.html'
+        }, name='password_change'
+    ),
+    url(
+        r'^password_change_done/$', views.password_change_done, {
+            'template_name': 'auth/password_change_done.html'
+        }, name='password_change_done'
     ),
     url(r'^', include('bank_admin.urls', namespace='bank_admin',)),
     url(r'^', include('feedback.urls')),
