11ba676c8a5a96c8c25d1162f89d7fe2f97c0240
Igor <igor.ushkarev@digital.justice.gov.uk>
2016-04-27 11:46:21 +0100

    Prevent robots from crawling the site

--- mtp_bank_admin/urls.py
+++ mtp_bank_admin/urls.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.conf.urls import include, url
 from django.core.urlresolvers import reverse_lazy
+from django.http import HttpResponse
 from django.views.generic import RedirectView
 
 from moj_auth import views
@@ -54,6 +55,7 @@ urlpatterns = [
     url(r'^healthcheck.json$', HealthcheckView.as_view(), name='healthcheck_json'),
 
     url(r'^favicon.ico$', RedirectView.as_view(url=settings.STATIC_URL + 'images/favicon.ico', permanent=True)),
+    url(r'^robots.txt$', lambda request: HttpResponse('User-agent: *\nDisallow: /', content_type='text/plain')),
 ]
 
 handler404 = 'mtp_common.views.page_not_found'
