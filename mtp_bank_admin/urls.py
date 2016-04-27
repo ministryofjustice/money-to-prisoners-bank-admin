58192d1ca26fab78193b091d74079cd8c6e6c470
Igor <igor.ushkarev@digital.justice.gov.uk>
2016-04-27 11:45:42 +0100

    Add favicon.ico redirect

--- mtp_bank_admin/urls.py
+++ mtp_bank_admin/urls.py
@@ -1,5 +1,7 @@
+from django.conf import settings
 from django.conf.urls import include, url
 from django.core.urlresolvers import reverse_lazy
+from django.views.generic import RedirectView
 
 from moj_auth import views
 from moj_irat.views import HealthcheckView, PingJsonView
@@ -50,6 +52,8 @@ urlpatterns = [
         version_number_key='APP_BUILD_TAG',
     ), name='ping_json'),
     url(r'^healthcheck.json$', HealthcheckView.as_view(), name='healthcheck_json'),
+
+    url(r'^favicon.ico$', RedirectView.as_view(url=settings.STATIC_URL + 'images/favicon.ico', permanent=True)),
 ]
 
 handler404 = 'mtp_common.views.page_not_found'
