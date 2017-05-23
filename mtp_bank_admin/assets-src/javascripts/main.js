4dd3677a848d93cacdc4947fa3f0200c437cba99
Igor <igor.ushkarev@digital.justice.gov.uk>
2017-05-23 15:18:29 +0100

    Update common dependency to use latest GOV.UK styles

--- mtp_bank_admin/assets-src/javascripts/main.js
+++ mtp_bank_admin/assets-src/javascripts/main.js
@@ -1,8 +1,8 @@
 (function () {
   'use strict';
+  require('polyfills').Polyfills.init();
 
   require('proposition-header').PropositionHeader.init();
   require('analytics').Analytics.init();
   require('help-popup').HelpPopup.init();
-  require('selection-buttons').SelectionButtons.init();
 }());
