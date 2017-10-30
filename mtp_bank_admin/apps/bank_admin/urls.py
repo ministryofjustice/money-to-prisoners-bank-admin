f0a04f939837170c8a569341c39632e4bb10310b
Igor <igor.ushkarev@digital.justice.gov.uk>
2017-10-30 15:41:11 +0000

    Remove view subclass for QNA section as it's not customised

--- mtp_bank_admin/apps/bank_admin/urls.py
+++ mtp_bank_admin/apps/bank_admin/urls.py
@@ -1,20 +1,16 @@
 from django.contrib.auth.decorators import login_required
 from django.conf.urls import url
 from django.views.generic.base import TemplateView
-from bank_admin.views import QAndAView
 
 from . import views
 
 app_name = 'bank_admin'
 urlpatterns = [
-    url(r'^$', login_required(TemplateView.as_view(
-        template_name='bank_admin/dashboard.html')),
-        name='dashboard'),
-    url(r'^refund_pending/download/$', views.download_refund_file,
-        name='download_refund_file'),
-    url(r'^adi/download/$', views.download_adi_journal,
-        name='download_adi_journal'),
-    url(r'^bank_statement/download/$', views.download_bank_statement,
-        name='download_bank_statement'),
-    url(r'^q_and_a/$', QAndAView.as_view(), name='q_and_a')
+    url(r'^$', login_required(TemplateView.as_view(template_name='bank_admin/dashboard.html')), name='dashboard'),
+
+    url(r'^refund_pending/download/$', views.download_refund_file, name='download_refund_file'),
+    url(r'^adi/download/$', views.download_adi_journal, name='download_adi_journal'),
+    url(r'^bank_statement/download/$', views.download_bank_statement, name='download_bank_statement'),
+
+    url(r'^q_and_a/$', TemplateView.as_view(template_name='q_and_a/q_and_a.html'), name='q_and_a')
 ]
