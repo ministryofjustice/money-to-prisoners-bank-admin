6248f99322c9c36e6baa2b5f4615e306bb558790
Ian Brechin <ian.brechin@gmail.com>
2017-12-07 17:30:38 +0000

    Start implementation for disbursements journal generation

---
+++ mtp_bank_admin/apps/bank_admin/disbursements_config.py
@@ -0,0 +1,142 @@
+DISBURSEMENTS_JOURNAL_SHEET = 'Sheet1'
+DISBURSEMENTS_JOURNAL_START_ROW = 19
+
+
+DISBURSEMENT_FIELDS = {
+    'operating_unit': {
+        'column': 'A',
+        'value': 'MOJ'
+    },
+    'supplier_number': {
+        'column': 'B',
+        'value': ''
+    },
+    'site_name': {
+        'column': 'C',
+        'value': '{prison_name}'
+    },
+    'payee_type': {
+        'column': 'D',
+        'value': '?'
+    },
+    'unique_payee_reference': {
+        'column': 'E',
+        'value': '{recipient_id}'
+    },
+    'payee_fornemae': {
+        'column': 'F',
+        'value': '{recipient_forename}'
+    },
+    'payee_surname': {
+        'column': 'G',
+        'value': '{recipient_surname}'
+    },
+    'payee_address_line1': {
+        'column': 'H',
+        'value': '{line1}'
+    },
+    'payee_address_line2': {
+        'column': 'I',
+        'value': '{line2}'
+    },
+    'payee_address_city': {
+        'column': 'J',
+        'value': '{city}'
+    },
+    'payee_postcode': {
+        'column': 'K',
+        'value': '{postcode}'
+    },
+    'remittance_email_address': {
+        'column': 'L',
+        'value': '{recipient_email}'
+    },
+    'vat_registration_number': {
+        'column': 'M',
+        'value': ''
+    },
+    'payment_method': {
+        'column': 'N',
+        'value': '{payment_method}'
+    },
+    'sort_code': {
+        'column': 'O',
+        'value': '{sort_code}'
+    },
+    'account_number': {
+        'column': 'P',
+        'value': '{account_number}'
+    },
+    'name_of_bank': {
+        'column': 'Q',
+        'value': ''
+    },
+    'account_name': {
+        'column': 'R',
+        'value': '{recipient_forename} {recipient_surname}'
+    },
+    'roll_number': {
+        'column': 'S',
+        'value': '{roll_number}'
+    },
+    'invoice_date': {
+        'column': 'T',
+        'value': '{date}'
+    },
+    'invoice_number': {
+        'column': 'U',
+        'value': ''
+    },
+    'description': {
+        'column': 'V',
+        'value': '{description}'
+    },
+    'entity': {
+        'column': 'W',
+        'value': '0210'
+    },
+    'cost_centre': {
+        'column': 'X',
+        'value': '{prison_ledger_code}'
+    },
+    'account': {
+        'column': 'Y',
+        'value': '?'
+    },
+    'objective': {
+        'column': 'Z',
+        'value': '0000000'
+    },
+    'analysis': {
+        'column': 'AA',
+        'value': '00000000'
+    },
+    'vat_rate': {
+        'column': 'AB',
+        'value': '0'
+    },
+    'line_description': {
+        'column': 'AC',
+        'value': ''
+    },
+    'net_amount': {
+        'column': 'AD',
+        'value': '{amount}'
+    },
+    'vat_amount': {
+        'column': 'AE',
+        'value': '0'
+    },
+    'total_amount': {
+        'column': 'AF',
+        'value': '{amount}'
+    },
+    'completer_id': {
+        'column': 'AG',
+        'value': ''
+    },
+    'approver_id': {
+        'column': 'AH',
+        'value': ''
+    },
+}
