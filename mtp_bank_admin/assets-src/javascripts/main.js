b2915ce0d24b7a752f333f9db8e0239b26230eaa
Igor <igor.ushkarev@digital.justice.gov.uk>
2018-04-10 12:40:24 +0100

    Add email domain name correction suggestion to email input fields

--- mtp_bank_admin/assets-src/javascripts/main.js
+++ mtp_bank_admin/assets-src/javascripts/main.js
@@ -5,4 +5,9 @@
   require('proposition-user-menu').PropositionUserMenu.init();
   require('analytics').Analytics.init();
   require('disclosure').Disclosure.init();
+  require('mailcheck-warning').MailcheckWarning.init(
+    '.mtp-account-management input[type=email]',
+    ['sscl.gse.gov.uk', 'hmps.gsi.gov.uk', 'noms.gsi.gov.uk', 'justice.gsi.gov.uk'],
+    ['gse.gov.uk', 'gsi.gov.uk', 'gov.uk']
+  );
 }());
