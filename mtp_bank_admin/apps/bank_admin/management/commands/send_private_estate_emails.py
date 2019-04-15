52425991ce7196d522f41b1279b0bdb88fd6c82e
Igor <igor.ushkarev@digital.justice.gov.uk>
2019-04-15 16:36:37 +0100

    Use 4-digit year in private estate CSV batches …to try to get the CMS to parse dates accurately

--- mtp_bank_admin/apps/bank_admin/management/commands/send_private_estate_emails.py
+++ mtp_bank_admin/apps/bank_admin/management/commands/send_private_estate_emails.py
@@ -113,7 +113,7 @@ class Command(BaseCommand):
         total = 0
         count = 0
         for batch in batches:
-            csv_batch_date = batch['date'].strftime('%d/%m/%y')
+            csv_batch_date = batch['date'].strftime('%d/%m/%Y')
             credit_list = retrieve_all_pages_for_path(
                 self.api_session,
                 'private-estate-batches/%s/%s/credits/' % (
