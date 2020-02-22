9c91536e8c6f66292296d70ab3926e8392d0b213
Igor <igor.ushkarev@digital.justice.gov.uk>
2020-02-22 15:10:42 +0000

    Add missing notifications JS so that they are expandable like in other apps

--- mtp_bank_admin/assets-src/javascripts/main.js
+++ mtp_bank_admin/assets-src/javascripts/main.js
@@ -4,6 +4,7 @@
   // common
   require('analytics').Analytics.init();
   require('disclosure').Disclosure.init();
+  require('notifications').Notifications.init();
   require('mailcheck-warning').MailcheckWarning.init(
     '.mtp-account-management input[type=email]',
     ['sscl.gse.gov.uk', 'justice.gov.uk'],
