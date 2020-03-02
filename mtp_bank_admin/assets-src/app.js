019e4db75d35ad52da49488654d4935c382a27be
Igor <igor.ushkarev@digital.justice.gov.uk>
2020-03-02 17:57:17 +0000

    Rearrange source assets into single tree like govuk-frontend

--- mtp_bank_admin/assets-src/app.js
+++ mtp_bank_admin/assets-src/app.js
@@ -7,12 +7,10 @@ initAll();
 // mtp-common
 import {Analytics} from 'mtp/components/analytics';
 import {Banner} from 'mtp/components/banner';
-import {Disclosure} from 'mtp/components/disclosure';
 import {MailcheckWarning} from 'mtp/components/mailcheck-warning';
 
 Analytics.init();
 Banner.init();
-Disclosure.init();
 MailcheckWarning.init(
   '.mtp-account-management input[type=email]',
   ['gov.sscl.com', 'justice.gov.uk'],
