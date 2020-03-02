072feb72d1658d8b53b319b2fd2a17be19565caf
Igor <igor.ushkarev@digital.justice.gov.uk>
2020-03-02 17:51:33 +0000

    Use ES6 importing/exporting of JS modules

--- mtp_bank_admin/assets-src/javascripts/main.js
+++ mtp_bank_admin/assets-src/javascripts/main.js
@@ -1,13 +1,20 @@
-(function () {
-  'use strict';
+'use strict';
 
-  // common
-  require('analytics').Analytics.init();
-  require('disclosure').Disclosure.init();
-  require('notifications').Notifications.init();
-  require('mailcheck-warning').MailcheckWarning.init(
-    '.mtp-account-management input[type=email]',
-    ['gov.sscl.com', 'justice.gov.uk'],
-    ['gov.sscl.com', 'gov.uk']
-  );
-}());
+// design systems
+import {initAll} from 'govuk-frontend';
+initAll();
+
+// mtp-common
+import {Analytics} from 'mtp/components/analytics';
+import {Banner} from 'mtp/components/banner';
+import {Disclosure} from 'mtp/components/disclosure';
+import {MailcheckWarning} from 'mtp/components/mailcheck-warning';
+
+Analytics.init();
+Banner.init();
+Disclosure.init();
+MailcheckWarning.init(
+  '.mtp-account-management input[type=email]',
+  ['gov.sscl.com', 'justice.gov.uk'],
+  ['gov.sscl.com', 'gov.uk']
+);
