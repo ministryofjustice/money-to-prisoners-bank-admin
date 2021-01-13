f9765c78bf064ab762156b30583e5b1a371844ec
Igor <igor.ushkarev@digital.justice.gov.uk>
2021-01-13 02:14:15 +0000

    Use staff app components from mtp-common

--- mtp_bank_admin/assets-src/app.js
+++ mtp_bank_admin/assets-src/app.js
@@ -6,4 +6,19 @@ initAll();
 
 // mtp common components
 import {initDefaults} from 'mtp_common';
+import {initStaffDefaults} from 'mtp_common/staff-app';
+import {MailcheckWarning} from 'mtp_common/components/mailcheck-warning';
 initDefaults();
+initStaffDefaults();
+MailcheckWarning.init(
+  '.mtp-account-management input[type=email]',
+  ['gov.sscl.com', 'justice.gov.uk'],
+  ['gov.sscl.com', 'gov.uk'],
+  []
+);
+MailcheckWarning.init(
+  '#change-your-email #id_email',
+  ['gov.sscl.com', 'justice.gov.uk'],
+  ['gov.sscl.com', 'gov.uk'],
+  []
+);
