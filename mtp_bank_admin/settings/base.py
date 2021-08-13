5b6333774711b7f8f3e16839263e3474c77bb34c
Igor <igor.ushkarev@digital.justice.gov.uk>
2021-08-13 15:01:34 +0100

    Switch from using Mailgun (via django-anymail) to GOV.UK Notify and install template-checking periodic task

--- mtp_bank_admin/settings/base.py
+++ mtp_bank_admin/settings/base.py
@@ -54,7 +54,6 @@ INSTALLED_APPS = (
     'django.contrib.auth',
 )
 PROJECT_APPS = (
-    'anymail',
     'mtp_common',
     'mtp_common.metrics',
     'widget_tweaks',
@@ -265,18 +264,11 @@ ZENDESK_CUSTOM_FIELDS = {
     'contact_email': 30769508,
 }
 
-EMAIL_BACKEND = 'anymail.backends.mailgun.EmailBackend'
-ANYMAIL = {
-    'MAILGUN_API_KEY': os.environ.get('MAILGUN_ACCESS_KEY', ''),
-    'MAILGUN_SENDER_DOMAIN': os.environ.get('MAILGUN_SERVER_NAME', ''),
-    'MAILGUN_API_URL': os.environ.get('MAILGUN_API_URL', 'https://api.mailgun.net/v3'),
-    'SEND_DEFAULTS': {
-        'tags': [APP, ENVIRONMENT],
-    },
-}
-MAILGUN_FROM_ADDRESS = os.environ.get('MAILGUN_FROM_ADDRESS', '')
-if MAILGUN_FROM_ADDRESS:
-    DEFAULT_FROM_EMAIL = MAILGUN_FROM_ADDRESS
+GOVUK_NOTIFY_API_KEY = os.environ.get('GOVUK_NOTIFY_API_KEY', '')
+GOVUK_NOTIFY_REPLY_TO_PUBLIC = os.environ.get('GOVUK_NOTIFY_REPLY_TO_PUBLIC', '')
+GOVUK_NOTIFY_REPLY_TO_STAFF = os.environ.get('GOVUK_NOTIFY_REPLY_TO_STAFF', '')
+# install GOV.UK Notify fallback for emails accidentally sent using Django's email functionality:
+EMAIL_BACKEND = 'mtp_common.notify.email_backend.NotifyEmailBackend'
 
 SHOW_LANGUAGE_SWITCH = os.environ.get('SHOW_LANGUAGE_SWITCH', 'False') == 'True'
 
