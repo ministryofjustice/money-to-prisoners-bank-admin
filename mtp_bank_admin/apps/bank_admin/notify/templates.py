9d1af42d214f6e1456afb245ef8f177bc977f178
Igor <igor.ushkarev@digital.justice.gov.uk>
2021-09-21 14:31:53 +0100

    Improve copy of email sent to private estate with credits

--- mtp_bank_admin/apps/bank_admin/notify/templates.py
+++ mtp_bank_admin/apps/bank_admin/notify/templates.py
@@ -11,16 +11,17 @@ class BankAdminNotifyTemplates(NotifyTemplateRegistry):
         'bank-admin-private-csv': {
             'subject': 'Credits received from ‘Send money to someone in prison’ for ((prison_name)) on ((date))',
             'body': textwrap.dedent("""
-These credits were received for ((prison_name)) on ((date)) from ‘Send money to someone in prison’.
+This link contains the details of credits received for ((prison_name))
+on ((date)) from ‘Send money to someone in prison’.
 
-Please download this file and upload into the CMS:
 ((attachment))
 
-Make sure NO credits are rejected.
+* Click on the link to download the file
+* Upload it to your CMS
+* Make sure that no credits are rejected
+* You don’t have to send a response email to Secure Payment Service
 
-If there’s any doubt, put credits on hold or speak with your security department.
-
-Once all credits are accepted, make sure you DON’T send the response email to Secure Payment Service.
+If you have any doubts, you should put the credits on hold or contact your security department.
             """).strip(),
             'personalisation': [
                 'prison_name', 'date',
